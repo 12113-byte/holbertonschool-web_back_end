91a20d3c6dcffb1e75c19c77e8280729ae403582
Joleen <12113@holbertonstudents.com>
2026-05-04 11:58:45 +0000

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// Module that asks for a name, let's you input your name and ends afterwards
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+    console.log(`Your name is: ${data.toString().trim()}`);
+});
+
+process.stdin.on('close', () => {
+    console.log('This important software is now closing');
+});
