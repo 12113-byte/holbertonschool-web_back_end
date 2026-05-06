b57e882d1830df378d30be6f1b59d8f7a90e93bc
Joleen <12113@holbertonstudents.com>
2026-05-06 10:18:56 +0000

    Task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+// function that reads a file asynchronously
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the databse'));
+        return;
+      }
+      const lines = data.split('\n');
+      const noemptylines = lines.filter((lines) => lines.trim() !== '');
+      const listwithoutfirstline = noemptylines.slice(1);
+
+      const studentsincs = [];
+      const studentsinswe = [];
+
+      listwithoutfirstline.forEach((item) => {
+        const student = item.split(',');
+        if (student[3] === 'CS') {
+          studentsincs.push(student[0]);
+        } else if (student[3] === 'SWE') {
+          studentsinswe.push(student[0]);
+        }
+      });
+      console.log(`Number of students: ${listwithoutfirstline.length}`);
+      console.log(`Number of students in CS: ${studentsincs.length}. List: ${studentsincs.join(', ')}`);
+      console.log(`Number of students in SWE: ${studentsinswe.length}. List: ${studentsinswe.join(', ')}`);
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
