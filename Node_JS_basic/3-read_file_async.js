163a2615efef7ef6c54ebf68e626d12bf9846a62
Joleen <12113@holbertonstudents.com>
2026-05-06 11:39:03 +0000

    fixed typo

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -6,7 +6,7 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the databse'));
+        reject(new Error('Cannot load the database'));
         return;
       }
       const lines = data.split('\n');
