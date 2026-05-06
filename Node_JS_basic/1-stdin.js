9ab38f8d8405c95cab603fa0327c6016f0251717
Joleen <12113@holbertonstudents.com>
2026-05-06 04:56:50 +0000

    changed process.exit(1)

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,9 +5,9 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
   console.log('This important software is now closing');
-  process.exit();
+  process.exit(1);
 });
 
-// process.stdin.on('exit', () => {
-//   console.log('This important software is now closing');
-// });
+process.stdin.on('close', () => {
+  console.log('This important software is now closing');
+});
