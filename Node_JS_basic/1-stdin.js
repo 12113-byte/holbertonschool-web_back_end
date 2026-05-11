6963a4a22608515aa14863e298be72cfae37f6bf
Joleen <12113@holbertonstudents.com>
2026-05-11 06:00:48 +0000

    changed console.log to process.stdout.arite

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 // Module that asks for a name, let's you input your name and ends afterwards
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
-  console.log('This important software is now closing');
-  process.exit(1);
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  // process.exit(1);
 });
 
 process.stdin.on('close', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
