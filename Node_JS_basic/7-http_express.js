20be2b57d07c1cd430e45324303b23cd7888f114
Joleen <12113@holbertonstudents.com>
2026-05-17 09:19:42 +0000

    Task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+  .then((result) => {
+    res.send(`This is the list of our students\n${result}`);
+  })
+  .catch((error) => {
+    res.status(500).send(error.message);
+  })
+});
+
+app.listen(1245, () => {
+
+});
+
+module.exports = app;
